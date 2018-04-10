fc938b34bf13ff255b5ce7f26254680add3d6cd1
julien <julien.karadayi@gmail.com>
2018-04-10 15:27:35 +0200

    rename project fineDi

--- flaskr/db.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-import click
-
-
-def connect_db():
-    """Connects to the specific database."""
-    rv = sqlite3.connect(app.config['DATABASE'])
-    rv.row_factory = sqlite3.Row
-    return rv
--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup
 
 setup(
-    name='flaskr',
-    packages=['flaskr'],
+    name='fineDi',
+    packages=['fineDi'],
     include_package_data=True,
     install_requires=[
         'flask',
