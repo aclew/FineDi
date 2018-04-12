41fb3ddc51d6927d522a4ae892f196b9383df579
julien <julien.karadayi@gmail.com>
2018-04-12 09:56:42 +0200

    get prev/next wav when available

--- fineDi/diarization_refinement.py
+++ fineDi/diarization_refinement.py
@@ -17,21 +17,73 @@ app.config.update(dict(
         PASSWORD='default'
         ))
 
-all_wavs=['test1.wav', 'test2.wav']
-cur_wav= -1
-print cur_wav
+#all_wavs=['test1.wav', 'test2.wav']
+#cur_wav= -1
+#print cur_wav
 #app.config.from_envvar('FLASKR_SETTINGS', silent=True)
 
-@app.route('/', methods=['GET', 'POST'])
-def show_entries():
+@app.route('/')
+def index():
+    """ TODO DEFINE
+        Let the user choose what routine (s)he wants to use
+        to refine the diarization. 
+        Suggested routines: - treat all wavs
+                                - which transcription to use ? gold ? Diartk ?
+                            - treat one wav
+                                - give name
+                                - which transcription to use ? gold ? Diartk ?
+                            - upload one wav + transcription
+    """
+    return "hello world"
+
+@app.route('/all_wavs/<wav_name>', methods=['GET', 'POST'])
+def treat_all_wavs(wav_name='test1.wav'):
+    """ This function creates the app
+        when the user is treating all the wavs. 
+        It gets the current wav but also the previous and the next.
+        This page can be accessed in the browser by going directly 
+        to localhost/all_wavs/<wav_name> , where wav_name is
+        the name of the wav you want to treat. 
+
+        TODO: add what kind of transcription you want to treat.
+    """
     #entries = cur.fetchall()
+    wav_list = get_wav_list()
     
-    entries=["bel"]
-    print cur_wav
-    cur_wav += 1
-    wav = "audio/" + all_wavs[cur_wav]
+    # try to get the position of current wav in list
+    try:
+        wav_index = wav_list.index(wav_name)
+        print wav_list
+        print wav_index
+        # get previous wav
+        if wav_index > 0:
+            prev_wav = wav_list[wav_index - 1]
+        else:
+            prev_wav = None
+        
+        # get next wav
+        if wav_index < len(wav_list) - 1:
+            next_wav = wav_list[wav_index + 1]
+        else:
+            next_wav = None
+    except:
+        # if the current wav is not in list, throw error page
+        pass # TODO create error page
+
+    # labels that can be put to the segment
+    entries=[" laugh", " cry", " speech", " do not change annotation"]
+    
+    #wav = "audio/" + wav_name
+    #wav = os.path.join(app.instance_path, 'static', 'audio', wav_name)
     print(request.form.getlist('trs_label'))
-    return render_template('show_entries.html', entries=entries, wav=wav)
+    return render_template('show_entries.html', entries=entries, wav=wav_name,
+                           next_wav=next_wav, prev_wav=prev_wav)
 
 
-
+def get_wav_list():
+    """ Get all the wav files in the static folder
+        and return them as a list.
+        Useful when routine used is to treat all the wavs.
+    """
+    print app.root_path
+    return os.listdir(os.path.join(app.root_path, 'static', 'audio'))
