8458442179387d025014cfeead6a1ef344df91e6
julien <julien.karadayi@gmail.com>
2018-04-27 14:40:36 +0200

    add some infos on the wav files

---
+++ config.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+#
+"""
+Config file for fineDi, the diarization refinement application
+"""
+DATA_ROOT='/vagrant/data'
+TEMP_ROOT='/vagrant/fine_di/temp'
+SECRET_KEY='development key'
+USERNAME='admin'
+PASSWORD='default'
+MEDIA_ROOT='static/media'
+
