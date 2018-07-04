cfd8b5aa31939281635b5109ab55cdc6ba22ec4c
Laure Elzière <Laure Elzire@MacBook-Pro-de-Laure.local>
2018-07-04 14:29:03 +0200

    first commit

--- launch_app.sh
+++ launch_app.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #
 # This script is useful to launch the app
+source activate FineDi
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 echo $DIR
 
@@ -22,20 +23,28 @@ fi
 
 # run app
 flask run&
+echo "press x to exit"
+read year
+
+if (( "$year" == "x" )); then
+    
 # wait a bit for the app to launch
-sleep 2
-
-# get flask pid
-flask_pid=`ps | grep flask | awk '{print $1}'`
-
-# open app in firefox
-firefox 127.0.0.1:5000&
-# wait again for firefox to launch
-sleep 5
-
-# get firefox pid
-firefox_pid=`ps | grep firefox | awk '{print $1}'`
-
-# wait for the firefox window to close and then kill the server that runs flask
-wait $firefox_pid
-kill -15 $flask_pid
+#sleep 2
+#
+## get flask pid
+    flask_pid=`ps | grep flask | awk '{print $1}'`
+#
+## open app in firefox
+#firefox 127.0.0.1:5000&
+## wait again for firefox to launch
+#sleep 5
+#
+## get firefox pid
+#firefox_pid=`ps | grep firefox | awk '{print $1}'`
+#
+## wait for the firefox window to close and then kill the server that runs flask
+#wait $firebox_pid
+#
+#
+    kill -15 $flask_pid
+fi
